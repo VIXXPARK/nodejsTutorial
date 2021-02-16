587ab738f5a4447de2fe65720f261c522f7416da
Suhan Park <vixx170627@gmail.com>
2021-02-16 23:02:21 +0900

    user id 를 이용하여 특정 데이터 추출중

--- dearborn/routes/api/post/controller.js
+++ dearborn/routes/api/post/controller.js
@@ -5,9 +5,8 @@ const disLike = require('../../../models/dislike');
 const jwt_decode = require('jwt-decode');
 
 exports.uploadPost = (req,res)=>{
-    var decoded=jwt_decode(req.headers['x-access-token']) 
     const {title,content,thumbnail,images} = req.body
-    const userId = decoded._id
+    const userId = req.decoded._id
     console.log(userId)
     Post.create(title,content,thumbnail,images,userId)
         .then(res.json({
@@ -54,9 +53,34 @@ exports.getPost = (req,res)=>{
         if(limit>posts.length-offset){
             limit=posts.length-offset
         }
+        let jsonPost= new Array();
+        for(step=offset;step<offset+limit;step++){
+            var postItem= new Object();
+            var nickname='';
+            var profile='';
+            let user_id = posts[step]["user"];
+            postItem.view=posts[step]["view"];
+            postItem.scope=posts[step]["scope"];
+            postItem.category=posts[step]["category"];
+            postItem.score=posts[step]["score"];
+            postItem._id=posts[step]["_id"];
+            postItem.title=posts[step]["title"];
+            postItem.content=posts[step]["content"];
+            postItem.thumbnail=posts[step]["thumbnail"];
+            postItem.images = posts[step]["images"];
+            var info = [];
+            User.findById(user_id).exec(function(err,results){
+                info.push(results);
+                console.log(results.nickname)
+            });
+            console.log(info)
+            // postItem.profile=info.profile;
+            // postItem.nickname=info.nickname;
+            jsonPost.push(postItem);
+        }
         res.json({
             success:true,
-            "post": posts.slice(offset,offset+limit),
+            "post": jsonPost,
             "Nextoffset":offset+limit
         })
     })
@@ -101,17 +125,16 @@ exports.getProfile = (req,res,next)=>{
 }
 
 exports.likeUp =(req,res,next)=>{
-    var decoded=jwt_decode(req.headers['x-access-token'])
     var like=new Like();
-    like.userId=decoded._id
+    like.userId=req.decoded._id
     like.postId=req.body.postId
-    Like.count({userId:decoded._id,postId:req.body.postId},function(err,num){
+    Like.count({userId:req.decoded._id,postId:req.body.postId},function(err,num){
         if (num==0){
             Post.findById(req.body.postId)
             .then((posts)=>{
                 console.log(posts.user)
-                console.log(decoded._id)
-                if(posts.user==decoded._id){
+                console.log(req.decoded._id)
+                if(posts.user==req.decoded._id){
                     res.json({message:"not authenticated!!"})
                 }
                 else{
@@ -140,8 +163,7 @@ exports.likeUp =(req,res,next)=>{
 
 }
 exports.likeDown =(req,res,next)=>{
-    var decoded=jwt_decode(req.headers['x-access-token']) 
-    Like.remove({userId:decoded._id,postId:req.body.postId})
+    Like.remove({userId:req.decoded._id,postId:req.body.postId})
     .then((like)=>{
         res.json({
             success:true,
@@ -152,11 +174,10 @@ exports.likeDown =(req,res,next)=>{
 
 
 exports.dislikeUp = (req,res,next)=>{
-    var decoded=jwt_decode(req.headers['x-access-token'])
     var result = new disLike();
-    result.userId=decoded._id;
+    result.userId=req.decoded._id;
     result.postId=req.body.postId;
-    Like.remove({userId:decoded._id,postId:req.body.postId})
+    Like.remove({userId:req.decoded._id,postId:req.body.postId})
     .then((data)=>{
         result.save()
         console.log(data)
@@ -168,8 +189,7 @@ exports.dislikeUp = (req,res,next)=>{
 }
 
 exports.dislikedown = (req,res,next)=>{
-    var decoded=jwt_decode(req.headers['x-access-token'])
-    disLike.remove({userId:decoded._id,postId:req.body.postId})
+    disLike.remove({userId:req.decoded._id,postId:req.body.postId})
     .then((result)=>{
         res.json({
             success:true,
@@ -179,8 +199,7 @@ exports.dislikedown = (req,res,next)=>{
 }
 
 exports.getlikePost = (req,res,next)=>{
-    var decoded=jwt_decode(req.headers['x-access-token'])
-    Like.find({userId:decoded._id})
+    Like.find({userId:req.decoded._id})
     .then((datas)=>{
         var limit = parseInt(req.query.limit) 
         var offset = parseInt(req.query.offset)
